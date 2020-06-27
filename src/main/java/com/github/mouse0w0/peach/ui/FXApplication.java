24cd2f15494f30a3a7c8b2738568ed3c08101605
Mouse <mouse0w0@qq.com>
2020-06-27 18:29:33 +0800

    feat: update WelcomeUI

--- src/main/java/com/github/mouse0w0/peach/ui/FXApplication.java
+++ src/main/java/com/github/mouse0w0/peach/ui/FXApplication.java
@@ -1,14 +1,11 @@
 package com.github.mouse0w0.peach.ui;
 
 import javafx.application.Application;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class FXApplication extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(new WelcomeUI()));
-        primaryStage.setTitle("🍑Peach");
-        primaryStage.show();
+        WelcomeUI.show(primaryStage);
     }
 }
--- src/main/java/com/github/mouse0w0/peach/ui/WelcomeUI.java
+++ src/main/java/com/github/mouse0w0/peach/ui/WelcomeUI.java
@@ -1,20 +1,43 @@
 package com.github.mouse0w0.peach.ui;
 
+import com.github.mouse0w0.eventbus.Listener;
 import com.github.mouse0w0.i18n.I18n;
+import com.github.mouse0w0.peach.Peach;
+import com.github.mouse0w0.peach.event.project.ProjectOpenedEvent;
 import com.github.mouse0w0.peach.project.ProjectManager;
 import com.github.mouse0w0.peach.ui.project.NewProjectUI;
 import com.github.mouse0w0.peach.ui.util.FXUtils;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 
 import java.io.File;
 
 public class WelcomeUI extends BorderPane {
 
+    private static Stage WELCOME_UI_STAGE;
+
     @FXML
     public ListView<String> recentProject;
+
+    static {
+        Peach.getEventBus().addListener(WelcomeUI::onOpenedProject);
+    }
+
+    public static void show(Stage primaryStage) {
+        WELCOME_UI_STAGE = primaryStage;
+        WELCOME_UI_STAGE.setScene(new Scene(new WelcomeUI()));
+        WELCOME_UI_STAGE.setTitle("🍑Peach");
+        WELCOME_UI_STAGE.show();
+    }
+
+    @Listener
+    public static void onOpenedProject(ProjectOpenedEvent event) {
+        WELCOME_UI_STAGE.hide();
+    }
 
     public WelcomeUI() {
         FXUtils.loadFXML(this, "ui/Welcome.fxml");
