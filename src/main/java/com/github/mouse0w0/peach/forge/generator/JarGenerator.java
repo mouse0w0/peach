8deaebf18b36ef423222b0b3c77e9287d930d5cb
Mouse <mouse0w0@qq.com>
2020-07-03 00:57:17 +0800

    feat: add ZipUtils

--- src/main/java/com/github/mouse0w0/peach/forge/generator/JarGenerator.java
+++ src/main/java/com/github/mouse0w0/peach/forge/generator/JarGenerator.java
@@ -1,7 +1,7 @@
 package com.github.mouse0w0.peach.forge.generator;
 
 import com.github.mouse0w0.peach.forge.ForgeProjectInfo;
-import com.github.mouse0w0.peach.util.JarUtils;
+import com.github.mouse0w0.peach.util.ZipUtils;
 
 import java.io.IOException;
 import java.nio.file.Path;
@@ -11,7 +11,7 @@ public class JarGenerator {
     public static void generate(CompileContext context) throws IOException {
         ForgeProjectInfo projectInfo = context.getData(ForgeCompiler.PROJECT_INFO_KEY);
         Path artifacts = context.getData(ForgeCompiler.ARTIFACTS_STORE_PATH).resolve(projectInfo.getId() + "-" + projectInfo.getVersion() + ".jar");
-        JarUtils.jar(artifacts,
+        ZipUtils.zip(artifacts,
                 context.getData(ForgeCompiler.CLASSES_STORE_PATH),
                 context.getData(ForgeCompiler.RESOURCES_STORE_PATH));
     }
--- src/main/java/com/github/mouse0w0/peach/util/ZipUtils.java
+++ src/main/java/com/github/mouse0w0/peach/util/ZipUtils.java
@@ -10,16 +10,16 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.jar.JarEntry;
-import java.util.jar.JarOutputStream;
+import java.util.zip.ZipOutputStream;
 
-public class JarUtils {
-    public static void jar(Path outputFile, Path... sources) throws IOException {
-        jar(outputFile, Arrays.asList(sources));
+public class ZipUtils {
+    public static void zip(Path outputFile, Path... sources) throws IOException {
+        zip(outputFile, Arrays.asList(sources));
     }
 
-    public static void jar(Path outputFile, Collection<Path> sources) throws IOException {
+    public static void zip(Path outputFile, Collection<Path> sources) throws IOException {
         FileUtils.createFileIfNotExists(outputFile);
-        try (JarOutputStream output = new JarOutputStream(Files.newOutputStream(outputFile))) {
+        try (ZipOutputStream output = new ZipOutputStream(Files.newOutputStream(outputFile))) {
             for (Path source : sources) {
                 Path root = Files.isRegularFile(source) ? source.getParent() : source;
                 Iterator<Path> iterator = Files.walk(source).iterator();
@@ -27,14 +27,14 @@ public class JarUtils {
                     Path file = iterator.next();
 
                     if (Files.isRegularFile(file)) {
-                        copyIntoJar(file, root.relativize(file).toString().replace('\\', '/'), output);
+                        copyFileIntoZip(file, root.relativize(file).toString().replace('\\', '/'), output);
                     }
                 }
             }
         }
     }
 
-    private static void copyIntoJar(Path file, String entryName, JarOutputStream output) throws IOException {
+    private static void copyFileIntoZip(Path file, String entryName, ZipOutputStream output) throws IOException {
         try (InputStream input = Files.newInputStream(file)) {
             output.putNextEntry(new JarEntry(entryName));
             IOUtils.copy(input, output);
