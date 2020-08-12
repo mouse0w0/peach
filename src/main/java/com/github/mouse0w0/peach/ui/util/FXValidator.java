5048882a83421f979fff0141b63a1f97bfa5ca9f
Mouse <mouse0w0@qq.com>
2020-08-12 21:08:27 +0800

    feat: update FXValidator

--- src/main/java/com/github/mouse0w0/peach/ui/util/FXValidator.java
+++ src/main/java/com/github/mouse0w0/peach/ui/util/FXValidator.java
@@ -8,12 +8,14 @@ import java.util.function.Predicate;
 public class FXValidator {
 
     public static boolean validate(TextInputControl control, String message, Predicate<String> validator) {
-        if (!validator.test(control.getText())) {
-            Messages.error(I18n.translate(message));
-            control.requestFocus();
-            control.selectAll();
-            return false;
-        }
-        return true;
+        if (validator.test(control.getText())) return true;
+        Messages.error(I18n.translate(message));
+        control.requestFocus();
+        control.selectAll();
+        return false;
+    }
+
+    public static boolean validateNotEmpty(TextInputControl control, String message) {
+        return validate(control, message, text -> !text.isEmpty());
     }
 }
