284c8c642c2c0f3e5aacd20a859fd5b78c5def2d
Mouse <mouse0w0@qq.com>
2020-08-29 21:59:31 +0800

    feat: add FourPos and EightPos

---
+++ src/main/java/com/github/mouse0w0/peach/geometry/EightPos.java
@@ -0,0 +1,28 @@
+package com.github.mouse0w0.peach.geometry;
+
+public enum EightPos {
+    TOP_LEFT(FourPos.TOP, FourPos.LEFT),
+    TOP_RIGHT(FourPos.TOP, FourPos.RIGHT),
+    BOTTOM_LEFT(FourPos.BOTTOM, FourPos.LEFT),
+    BOTTOM_RIGHT(FourPos.BOTTOM, FourPos.RIGHT),
+    LEFT_TOP(FourPos.LEFT, FourPos.TOP),
+    LEFT_BOTTOM(FourPos.LEFT, FourPos.BOTTOM),
+    RIGHT_TOP(FourPos.RIGHT, FourPos.TOP),
+    RIGHT_BOTTOM(FourPos.RIGHT, FourPos.BOTTOM);
+
+    private final FourPos primary;
+    private final FourPos secondary;
+
+    EightPos(FourPos primary, FourPos secondary) {
+        this.primary = primary;
+        this.secondary = secondary;
+    }
+
+    public FourPos getPrimary() {
+        return primary;
+    }
+
+    public FourPos getSecondary() {
+        return secondary;
+    }
+}
---
+++ src/main/java/com/github/mouse0w0/peach/geometry/FourPos.java
@@ -0,0 +1,5 @@
+package com.github.mouse0w0.peach.geometry;
+
+public enum FourPos {
+    TOP, BOTTOM, LEFT, RIGHT
+}
