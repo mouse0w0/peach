82d1daa87a2a5c9f6dda32018e9a08465483c0cc
Mouse <mouse0w0@qq.com>
2020-09-01 20:58:33 +0800

    feat: update ComponentManagerImpl

--- src/main/java/com/github/mouse0w0/peach/component/ComponentManagerImpl.java
+++ src/main/java/com/github/mouse0w0/peach/component/ComponentManagerImpl.java
@@ -8,6 +8,7 @@ import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
 import java.net.URL;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Supplier;
@@ -33,34 +34,51 @@ public abstract class ComponentManagerImpl extends DataHolderImpl implements Com
         }) : services.get(classOfT));
     }
 
-    @SuppressWarnings({"rawtypes", "unchecked"})
+
     protected void registerServices(URL url) {
         try {
             SAXReader reader = new SAXReader();
             Document document = reader.read(url);
             Element services = document.getRootElement();
-            for (Element service : services.elements("service")) {
-                String implementationName = service.attributeValue("implementation");
-                String interfaceName = service.attributeValue("interface");
-                boolean lazy = "true".equals(service.attributeValue("lazy"));
+            registerServices(services.elements("service"));
+        } catch (DocumentException e) {
+            throw new ServiceException("Cannot load services from " + url, e);
+        }
+    }
 
-                Class<?> implementationClass = Class.forName(implementationName);
-                Class serviceClass = interfaceName != null ? Class.forName(interfaceName) : implementationClass;
-                if (lazy) {
-                    Supplier<?> serviceFactory = () -> {
-                        try {
-                            return implementationClass.newInstance();
-                        } catch (ReflectiveOperationException e) {
-                            throw new ServiceException("Cannot initialize service class " + implementationClass.getName());
-                        }
-                    };
-                    registerServiceFactory(serviceClass, serviceFactory);
-                } else {
-                    registerService(serviceClass, implementationClass.newInstance());
-                }
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    protected void registerServices(Collection<Element> services) {
+        for (Element service : services) {
+            String implementationName = service.attributeValue("implementation");
+            String interfaceName = service.attributeValue("interface");
+            boolean lazy = "true".equals(service.attributeValue("lazy"));
+
+            Class<?> implementationClass;
+            try {
+                implementationClass = Class.forName(implementationName);
+            } catch (ClassNotFoundException e) {
+                throw new ServiceException("Not found service implementation class " + implementationName, e);
             }
-        } catch (DocumentException | ReflectiveOperationException e) {
-            throw new ServiceException("Cannot load services: " + url, e);
+            Class serviceClass;
+            try {
+                serviceClass = interfaceName != null ? Class.forName(interfaceName) : implementationClass;
+            } catch (ClassNotFoundException e) {
+                throw new ServiceException("Not found service interface class " + implementationName, e);
+            }
+
+            if (lazy) {
+                registerServiceFactory(serviceClass, () -> newInstance(implementationClass));
+            } else {
+                registerService(serviceClass, newInstance(implementationClass));
+            }
+        }
+    }
+
+    private <T> T newInstance(Class<T> implementationClass) {
+        try {
+            return implementationClass.newInstance();
+        } catch (ReflectiveOperationException e) {
+            throw new ServiceException("Failed to create the service instance of " + implementationClass.getName());
         }
     }
 }
