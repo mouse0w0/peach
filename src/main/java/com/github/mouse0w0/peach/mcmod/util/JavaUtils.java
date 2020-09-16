d9d494e48bff34f82f2e37f7e07126864ed1bebe
Mouse <mouse0w0@qq.com>
2020-09-16 23:31:29 +0800

    feat: update JavaUtils

--- src/main/java/com/github/mouse0w0/peach/mcmod/util/JavaUtils.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/util/JavaUtils.java
@@ -1,8 +1,25 @@
 package com.github.mouse0w0.peach.mcmod.util;
 
+import com.google.common.collect.Sets;
+
+import java.util.Set;
 import java.util.regex.Pattern;
 
 public class JavaUtils {
 
-    public static final Pattern CLASS_NAME = Pattern.compile("[a-zA-Z][a-zA-Z0-9_$]*");
+    public static final Set<String> RESERVED_WORDS = Sets.newHashSet(
+            "abstract", "assert", "boolean", "break", "byte", "case", "catch", "char", "class", "const",
+            "continue", "default", "do", "double", "else", "extends", "false", "final", "finally", "float",
+            "for", "goto", "if", "implements", "import", "instanceof", "int", "interface", "long", "native",
+            "new", "null", "package", "private", "protected", "public", "return", "short", "static", "strictfp",
+            "super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "void",
+            "volatile", "while");
+
+    public static final Pattern IDENTIFIER_PATTERN = Pattern.compile("[a-zA-Z][a-zA-Z0-9_$]*");
+
+    public static boolean isJavaIdentifier(String identifier) {
+        if (RESERVED_WORDS.contains(identifier)) return false;
+
+        return IDENTIFIER_PATTERN.matcher(identifier).matches();
+    }
 }
