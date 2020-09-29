e844df6b48a85fd391f5ecb49f297c4e1fa4028c
Mouse <mouse0w0@qq.com>
2020-09-29 20:08:09 +0800

    feat: update ElementType

--- src/main/java/com/github/mouse0w0/peach/mcmod/element/ElementType.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/element/ElementType.java
@@ -4,20 +4,17 @@ import com.github.mouse0w0.peach.project.Project;
 import com.github.mouse0w0.peach.wizard.Wizard;
 
 import java.nio.file.Path;
-import java.util.function.Supplier;
 
 public class ElementType<T extends Element> {
     private String name;
     private String translationKey;
     private Class<T> type;
-    private Supplier<T> elementFactory;
     private WizardFactory<T> wizardFactory;
 
-    public ElementType(String name, Class<T> type, Supplier<T> elementFactory, WizardFactory<T> wizardFactory) {
+    public ElementType(String name, Class<T> type, WizardFactory<T> wizardFactory) {
         this.name = name;
         this.translationKey = "mod.element." + name;
         this.type = type;
-        this.elementFactory = elementFactory;
         this.wizardFactory = wizardFactory;
     }
 
@@ -34,9 +31,13 @@ public class ElementType<T extends Element> {
     }
 
     public T createElement(Path file) {
-        T element = elementFactory.get();
-        Element.setFile(element, file);
-        return element;
+        try {
+            T element = type.newInstance();
+            Element.setFile(element, file);
+            return element;
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Cannot create element instance", e);
+        }
     }
 
     public Wizard createWizard(Project project, T element) {
--- src/main/java/com/github/mouse0w0/peach/mcmod/element/ElementTypes.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/element/ElementTypes.java
@@ -12,11 +12,11 @@ import com.github.mouse0w0.peach.mcmod.wizard.SmeltingRecipeWizard;
 public class ElementTypes {
 
     public static final ElementType<ItemElement> ITEM =
-            new ElementType<>("item", ItemElement.class, ItemElement::new, ItemWizard::new);
+            new ElementType<>("item", ItemElement.class, ItemWizard::new);
     public static final ElementType<ItemGroup> ITEM_GROUP =
-            new ElementType<>("item_group", ItemGroup.class, ItemGroup::new, ItemGroupWizard::new);
+            new ElementType<>("item_group", ItemGroup.class, ItemGroupWizard::new);
     public static final ElementType<CraftingRecipe> CRAFTING_RECIPE =
-            new ElementType<>("crafting_recipe", CraftingRecipe.class, CraftingRecipe::new, CraftingRecipeWizard::new);
+            new ElementType<>("crafting_recipe", CraftingRecipe.class, CraftingRecipeWizard::new);
     public static final ElementType<SmeltingRecipe> SMELTING_RECIPE =
-            new ElementType<>("smelting_recipe", SmeltingRecipe.class, SmeltingRecipe::new, SmeltingRecipeWizard::new);
+            new ElementType<>("smelting_recipe", SmeltingRecipe.class, SmeltingRecipeWizard::new);
 }
