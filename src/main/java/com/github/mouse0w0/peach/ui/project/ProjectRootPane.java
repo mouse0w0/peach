b5be649b6fd6d16af0ebd340510479765e137a3c
Mouse <mouse0w0@qq.com>
2020-10-31 00:43:02 +0800

    feat: support action description

--- src/main/java/com/github/mouse0w0/peach/ui/project/ProjectRootPane.java
+++ src/main/java/com/github/mouse0w0/peach/ui/project/ProjectRootPane.java
@@ -7,6 +7,7 @@ import com.github.mouse0w0.peach.action.IdeGroups;
 import com.github.mouse0w0.peach.project.Project;
 import com.github.mouse0w0.peach.ui.util.FXUtils;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
@@ -16,11 +17,15 @@ import javafx.stage.Window;
 
 class ProjectRootPane extends BorderPane {
 
+    private final Project project;
+
     private final MenuBar menuBar;
     private final TabPane tabPane;
     private final StatusBarImpl statusBar;
 
     public ProjectRootPane(Project project) {
+        this.project = project;
+
         FXUtils.addStyleSheet(this, "style/style.css");
 
         setPrefSize(820, 600);
@@ -32,6 +37,10 @@ class ProjectRootPane extends BorderPane {
         setBottom(statusBar.getContent());
 
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.ALL_TABS);
+    }
+
+    public Project getProject() {
+        return project;
     }
 
     public TabPane getTabPane() {
@@ -51,8 +60,51 @@ class ProjectRootPane extends BorderPane {
             if (!(child instanceof ActionGroup)) continue;
 
             Menu menu = actionManager.createMenu((ActionGroup) child);
+            setOnShownEventHandler(menu);
             menuBar.getMenus().add(menu);
         }
         return menuBar;
+    }
+
+    private static final EventHandler<Event> ON_MENU_SHOWN = new EventHandler<Event>() {
+        @Override
+        public void handle(Event event) {
+            Window window = FXUtils.getFocusedWindow().orElse(null);
+            if (window != null) {
+                ContextMenu contextMenu = (ContextMenu) window;
+                Parent root = contextMenu.getScene().getRoot();
+                for (Node node : root.lookupAll(".menu-item")) {
+                    node.setOnMouseEntered(ON_MOUSE_ENTERED);
+                    node.setOnMouseExited(ON_MOUSE_EXITED);
+                }
+            }
+        }
+    };
+
+    private static final EventHandler<? super MouseEvent> ON_MOUSE_ENTERED = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            Node node = (Node) event.getTarget();
+            MenuItem menuItem = (MenuItem) node.getProperties().get(MenuItem.class);
+            Action action = (Action) menuItem.getProperties().get(Action.class);
+            StatusBarInfo statusBarInfo = StatusBarInfo.getInstance(WindowManager.getInstance().getFocusedProject());
+            statusBarInfo.setText(action.getAppearance().getDescription());
+        }
+    };
+
+    private static final EventHandler<? super MouseEvent> ON_MOUSE_EXITED = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent event) {
+            StatusBarInfo.getInstance(WindowManager.getInstance().getFocusedProject()).setText("");
+        }
+    };
+
+    private static void setOnShownEventHandler(Menu menu) {
+        menu.setOnShown(ON_MENU_SHOWN);
+        for (MenuItem item : menu.getItems()) {
+            if (item instanceof Menu) {
+                setOnShownEventHandler((Menu) item);
+            }
+        }
     }
 }
