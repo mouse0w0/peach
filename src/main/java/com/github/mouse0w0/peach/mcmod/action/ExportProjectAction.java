123a615bb81606e9191d3a3cb279ee67bcc82580
Mouse <mouse0w0@qq.com>
2020-11-15 21:56:14 +0800

    feat: update ExportProjectAction

--- src/main/java/com/github/mouse0w0/peach/mcmod/action/ExportProjectAction.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/action/ExportProjectAction.java
@@ -3,6 +3,7 @@ package com.github.mouse0w0.peach.mcmod.action;
 import com.github.mouse0w0.i18n.I18n;
 import com.github.mouse0w0.peach.action.Action;
 import com.github.mouse0w0.peach.action.ActionEvent;
+import com.github.mouse0w0.peach.data.DataKeys;
 import com.github.mouse0w0.peach.mcmod.compiler.Compiler;
 import com.github.mouse0w0.peach.mcmod.project.McModSettings;
 import com.github.mouse0w0.peach.project.Project;
@@ -10,6 +11,8 @@ import com.github.mouse0w0.peach.ui.project.ProjectWindow;
 import com.github.mouse0w0.peach.ui.project.WindowManager;
 import com.github.mouse0w0.peach.ui.util.Alerts;
 import com.github.mouse0w0.peach.util.FileUtils;
+import com.github.mouse0w0.peach.util.Scheduler;
+import javafx.application.Platform;
 import javafx.stage.FileChooser;
 import org.apache.commons.lang3.SystemUtils;
 import org.slf4j.Logger;
@@ -18,35 +21,42 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.concurrent.CompletableFuture;
 
 public class ExportProjectAction extends Action {
     private static final Logger LOGGER = LoggerFactory.getLogger(ExportProjectAction.class);
 
     @Override
     public void perform(ActionEvent event) {
-        ProjectWindow window = WindowManager.getInstance().getFocusedWindow();
-        Project project = window.getProject();
-        Compiler compiler = new Compiler(project.getPath(), project.getPath().resolve("build"));
-        compiler.run();
+        Project project = DataKeys.PROJECT.get(event);
+        if (project == null) return;
 
-        McModSettings modSettings = compiler.getModSettings();
+        ProjectWindow window = WindowManager.getInstance().getWindow(project);
 
-        String fileName = modSettings.getId() + "-" + modSettings.getVersion() + ".jar";
+        CompletableFuture.supplyAsync(() -> {
+            Compiler compiler = new Compiler(project.getPath(), project.getPath().resolve("build"));
+            compiler.run();
+            return compiler;
+        }, Scheduler.computation()).thenAcceptAsync(compiler -> {
+            McModSettings modSettings = compiler.getModSettings();
 
-        FileChooser fileChooser = new FileChooser();
-        fileChooser.setTitle(I18n.translate("dialog.export_to.title"));
-        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Jar", "*.jar"));
-        fileChooser.setInitialFileName(fileName);
-        fileChooser.setInitialDirectory(SystemUtils.getUserHome());
-        File file = fileChooser.showSaveDialog(window.getStage());
-        if (file != null) {
-            try {
-                Path source = compiler.getOutputDirectory().resolve("artifacts/" + fileName);
-                FileUtils.forceCopy(source, file.toPath());
-            } catch (IOException e) {
-                LOGGER.error("Failed to export file: " + file, e);
-                Alerts.warning("dialog.export_to.failure");
+            String fileName = modSettings.getId() + "-" + modSettings.getVersion() + ".jar";
+
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle(I18n.translate("dialog.export_to.title"));
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Jar", "*.jar"));
+            fileChooser.setInitialFileName(fileName);
+            fileChooser.setInitialDirectory(SystemUtils.getUserHome());
+            File file = fileChooser.showSaveDialog(window.getStage());
+            if (file != null) {
+                try {
+                    Path source = compiler.getOutputDirectory().resolve("artifacts/" + fileName);
+                    FileUtils.forceCopy(source, file.toPath());
+                } catch (IOException e) {
+                    LOGGER.error("Failed to export file: " + file, e);
+                    Alerts.warning("dialog.export_to.failure");
+                }
             }
-        }
+        }, Platform::runLater);
     }
 }
