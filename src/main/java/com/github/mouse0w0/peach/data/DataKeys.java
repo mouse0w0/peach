540fc333b44c2ef0890f1ca8388a8f9d760a3b5d
Mouse <mouse0w0@qq.com>
2020-11-17 20:21:23 +0800

    feat: add FILE_PATH to DataKeys

--- src/main/java/com/github/mouse0w0/peach/data/DataKeys.java
+++ src/main/java/com/github/mouse0w0/peach/data/DataKeys.java
@@ -2,6 +2,10 @@ package com.github.mouse0w0.peach.data;
 
 import com.github.mouse0w0.peach.project.Project;
 
+import java.nio.file.Path;
+
 public interface DataKeys {
     DataKey<Project> PROJECT = DataKey.create("Project");
+
+    DataKey<Path> FILE_PATH = DataKey.create("FilePath");
 }
