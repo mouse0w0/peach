4260f9b46c14a4eee957222c24e95c90418ab8f5
Mouse <mouse0w0@qq.com>
2020-11-26 15:28:40 +0800

    feat: support PasteAction

--- src/main/java/com/github/mouse0w0/peach/action/edit/PasteAction.java
+++ src/main/java/com/github/mouse0w0/peach/action/edit/PasteAction.java
@@ -1,11 +1,26 @@
 package com.github.mouse0w0.peach.action.edit;
 
+import com.github.mouse0w0.i18n.I18n;
 import com.github.mouse0w0.peach.action.Action;
 import com.github.mouse0w0.peach.action.ActionEvent;
+import com.github.mouse0w0.peach.data.DataKeys;
 import com.github.mouse0w0.peach.dialog.Alert;
+import com.github.mouse0w0.peach.dialog.ButtonType;
+import com.github.mouse0w0.peach.dialog.PasteDialog;
 import javafx.scene.input.Clipboard;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.List;
 
 public class PasteAction extends Action {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PasteAction.class);
+
     @Override
     public void update(ActionEvent event) {
         getAppearance().setDisable(!Clipboard.getSystemClipboard().hasFiles());
@@ -13,6 +28,42 @@ public class PasteAction extends Action {
 
     @Override
     public void perform(ActionEvent event) {
-        Alert.error("Unsupported operation");
+        Path path = DataKeys.PATH.get(event);
+        if (path == null) return;
+
+        Path folder = Files.isDirectory(path) ? path : path.getParent();
+
+        List<File> files = Clipboard.getSystemClipboard().getFiles();
+        boolean multiple = files.size() > 1;
+        boolean overwriteAll = false;
+        boolean skipAll = false;
+        for (File file : files) {
+            Path source = file.toPath();
+            Path target = folder.resolve(file.getName());
+            try {
+                if (Files.exists(target)) {
+                    if (skipAll) continue;
+                    if (!overwriteAll) {
+                        ButtonType buttonType = new PasteDialog(I18n.translate("dialog.paste.title"),
+                                I18n.format("dialog.paste.message", folder, file.getName()), multiple)
+                                .showAndWait().orElse(PasteDialog.SKIP);
+                        if (buttonType == PasteDialog.SKIP) {
+                            continue;
+                        } else if (buttonType == PasteDialog.SKIP_ALL) {
+                            skipAll = true;
+                            continue;
+                        } else if (buttonType == PasteDialog.OVERWRITE_ALL) {
+                            overwriteAll = true;
+                        }
+                    }
+                    Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+                } else {
+                    Files.copy(source, target);
+                }
+            } catch (IOException e) {
+                LOGGER.error("Failed to paste file because an exception has occurred.", e);
+                Alert.error(I18n.translate("dialog.paste.title"), e.toString());
+            }
+        }
     }
 }
---
+++ src/main/java/com/github/mouse0w0/peach/dialog/PasteDialog.java
@@ -0,0 +1,28 @@
+package com.github.mouse0w0.peach.dialog;
+
+import javafx.scene.control.ButtonBar;
+
+public class PasteDialog extends Alert {
+
+    public static final ButtonType OVERWRITE =
+            new ButtonType("dialog.button.overwrite", ButtonBar.ButtonData.OK_DONE);
+
+    public static final ButtonType SKIP =
+            new ButtonType("dialog.button.skip", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+    public static final ButtonType OVERWRITE_ALL =
+            new ButtonType("dialog.button.overwriteAll", ButtonBar.ButtonData.RIGHT);
+
+    public static final ButtonType SKIP_ALL =
+            new ButtonType("dialog.button.skipAll", ButtonBar.ButtonData.RIGHT);
+
+    public PasteDialog(String title, String text, boolean multiple) {
+        super(title, text);
+        getButtonBar().setButtonOrder(ButtonBar.BUTTON_ORDER_NONE);
+        if (multiple) {
+            getButtons().setAll(OVERWRITE, SKIP, OVERWRITE_ALL, SKIP_ALL);
+        } else {
+            getButtons().setAll(OVERWRITE, SKIP);
+        }
+    }
+}
