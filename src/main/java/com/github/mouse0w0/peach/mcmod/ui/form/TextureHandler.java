4b3fad6fb363a309f4b8f0a3ec662ef0949b3472
Mouse <mouse0w0@qq.com>
2020-12-10 23:48:19 +0800

    feat: update TextureHandler

--- src/main/java/com/github/mouse0w0/peach/mcmod/ui/form/TextureHandler.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/ui/form/TextureHandler.java
@@ -44,6 +44,36 @@ public interface TextureHandler {
         };
     }
 
+    static TextureHandler ofKeepExtension(Path root, Path store) {
+        return new TextureHandler() {
+            @Override
+            public File fromString(String texture) {
+                return new File(root.resolve(texture).toString());
+            }
+
+            @Override
+            public String toString(File file) {
+                return root.relativize(file.toPath()).toString();
+            }
+
+            @Override
+            public File copy(File file) {
+                Path myFile = file.toPath();
+
+                if (myFile.startsWith(root)) return file;
+
+                try {
+                    Path newFile = ResourceUtils.copyToLowerCaseFile(myFile, store.resolve(myFile.getFileName()));
+                    return newFile != null ? newFile.toFile() : null;
+                } catch (IOException e) {
+                    LOGGER.error("Failed to copy file because an exception has occurred.", e);
+                    Alert.error("An exception has occurred!");
+                    return null;
+                }
+            }
+        };
+    }
+
     File fromString(String texture);
 
     String toString(File file);
