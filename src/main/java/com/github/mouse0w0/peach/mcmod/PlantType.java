7ebb88d26bf472f138a2ad10b8786db606309158
Mouse <mouse0w0@qq.com>
2021-02-21 14:26:19 +0800

    feat: update PlantType

--- src/main/java/com/github/mouse0w0/peach/mcmod/PlantType.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/PlantType.java
@@ -1,11 +1,26 @@
 package com.github.mouse0w0.peach.mcmod;
 
-public enum PlantType {
+import com.github.mouse0w0.i18n.I18n;
+import com.google.common.base.CaseFormat;
+
+public enum PlantType implements Localizable {
+    NONE,
     PLAINS,
     DESERT,
     BEACH,
     CAVE,
     WATER,
     NETHER,
-    CROP
+    CROP;
+
+    private final String translationKey;
+
+    PlantType() {
+        translationKey = "plantType." + CaseFormat.UPPER_UNDERSCORE.to(CaseFormat.LOWER_CAMEL, name());
+    }
+
+    @Override
+    public String getLocalizedText() {
+        return I18n.translate(translationKey);
+    }
 }
