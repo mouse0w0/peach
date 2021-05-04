eadf7120932585cdb1f10344031df5570236d4b5
Mouse <mouse0w0@qq.com>
2021-05-04 17:20:19 +0800

    feat: localize the file names of folders and files

--- src/main/java/com/github/mouse0w0/peach/mcmod/file/McModFileAppearance.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/file/McModFileAppearance.java
@@ -1,5 +1,6 @@
 package com.github.mouse0w0.peach.mcmod.file;
 
+import com.github.mouse0w0.i18n.I18n;
 import com.github.mouse0w0.peach.file.FileAppearance;
 import com.github.mouse0w0.peach.file.FileCell;
 import com.github.mouse0w0.peach.icon.Icons;
@@ -7,25 +8,61 @@ import com.github.mouse0w0.peach.mcmod.element.ElementRegistry;
 import com.github.mouse0w0.peach.util.FileUtils;
 import com.github.mouse0w0.peach.util.StringUtils;
 
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
 
 public class McModFileAppearance implements FileAppearance {
+
+    private final Map<Path, String> translationKeyMap;
+
+    public McModFileAppearance() {
+        translationKeyMap = new HashMap<>();
+        translationKeyMap.put(Paths.get("build"), "mod.folder.build");
+        translationKeyMap.put(Paths.get("sources"), "mod.folder.source");
+        translationKeyMap.put(Paths.get("resources"), "mod.folder.resource");
+        translationKeyMap.put(Paths.get("resources/textures"), "mod.folder.texture");
+        translationKeyMap.put(Paths.get("resources/textures/armors"), "mod.folder.texture.armor");
+        translationKeyMap.put(Paths.get("resources/textures/blocks"), "mod.folder.texture.block");
+        translationKeyMap.put(Paths.get("resources/textures/items"), "mod.folder.texture.item");
+    }
+
     @Override
     public boolean accept(Path file) {
-        String fileName = FileUtils.getFileName(file);
-        if ("project.forge.json".equals(fileName)) return true;
-        return ElementRegistry.getInstance().getElementType(file) != null;
+        if (Files.isDirectory(file)) {
+            for (Path path : translationKeyMap.keySet()) {
+                if (file.endsWith(path)) return true;
+            }
+            return false;
+        } else {
+            String fileName = FileUtils.getFileName(file);
+            if ("project.forge.json".equals(fileName)) return true;
+
+            return ElementRegistry.getInstance().getElementType(file) != null;
+        }
     }
 
     @Override
     public void apply(Path file, FileCell cell) {
-        String fileName = FileUtils.getFileName(file);
-        if ("project.forge.json".equals(fileName)) {
-            cell.setText(fileName);
-            cell.setIcon(Icons.File.Forge);
+        if (Files.isDirectory(file)) {
+            for (Path path : translationKeyMap.keySet()) {
+                if (file.endsWith(path)) {
+                    cell.setText(I18n.translate(translationKeyMap.get(path)));
+                    cell.setIcon(Icons.File.Folder);
+                    return;
+                }
+            }
         } else {
-            cell.setText(StringUtils.substringBefore(fileName, '.'));
-            cell.setIcon(Icons.File.ModElement);
+            String fileName = FileUtils.getFileName(file);
+            if ("project.forge.json".equals(fileName)) {
+                cell.setText(I18n.translate("mod.file.metadata"));
+                cell.setIcon(Icons.File.Forge);
+            } else {
+                cell.setText(StringUtils.substringBefore(fileName, '.'));
+                cell.setIcon(Icons.File.ModElement);
+            }
         }
     }
 }
