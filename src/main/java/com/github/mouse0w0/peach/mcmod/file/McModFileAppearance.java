c325e760800a79719b044ef82c07e8a097efcf65
Mouse <mouse0w0@qq.com>
2022-05-07 20:32:56 +0800

    feat: update McModFileAppearance

--- src/main/java/com/github/mouse0w0/peach/mcmod/file/McModFileAppearance.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/file/McModFileAppearance.java
@@ -7,6 +7,8 @@ import com.github.mouse0w0.peach.icon.Icons;
 import com.github.mouse0w0.peach.mcmod.element.ElementRegistry;
 import com.github.mouse0w0.peach.mcmod.element.ElementType;
 import com.github.mouse0w0.peach.mcmod.project.McModMetadata;
+import com.github.mouse0w0.peach.project.Project;
+import com.github.mouse0w0.peach.project.ProjectManager;
 import com.github.mouse0w0.peach.util.FileUtils;
 import com.github.mouse0w0.peach.util.StringUtils;
 
@@ -18,35 +20,41 @@ import java.util.Map;
 
 public class McModFileAppearance implements FileAppearance {
 
-    private final Map<Path, String> translationKeyMap;
+    private final Map<Path, String> localizablePathMap = new HashMap<>();
 
     public McModFileAppearance() {
-        translationKeyMap = new HashMap<>();
-        translationKeyMap.put(Paths.get("build"), "mod.folder.build");
-        translationKeyMap.put(Paths.get("sources"), "mod.folder.source");
-        translationKeyMap.put(Paths.get("resources"), "mod.folder.resource");
-        translationKeyMap.put(Paths.get("resources/textures"), "mod.folder.texture");
-        translationKeyMap.put(Paths.get("resources/textures/armor"), "mod.folder.texture.armor");
-        translationKeyMap.put(Paths.get("resources/textures/armors"), "mod.folder.texture.armor");
-        translationKeyMap.put(Paths.get("resources/textures/block"), "mod.folder.texture.block");
-        translationKeyMap.put(Paths.get("resources/textures/blocks"), "mod.folder.texture.block");
-        translationKeyMap.put(Paths.get("resources/textures/item"), "mod.folder.texture.item");
-        translationKeyMap.put(Paths.get("resources/textures/items"), "mod.folder.texture.item");
-        translationKeyMap.put(Paths.get("resources/textures/gui"), "mod.folder.texture.gui");
+        localizablePathMap.put(Paths.get("build"), "mod.folder.build");
+        localizablePathMap.put(Paths.get("sources"), "mod.folder.source");
+        localizablePathMap.put(Paths.get("resources"), "mod.folder.resource");
+        localizablePathMap.put(Paths.get("resources/models"), "mod.folder.model");
+        localizablePathMap.put(Paths.get("resources/models/block"), "mod.folder.model.item");
+        localizablePathMap.put(Paths.get("resources/models/item"), "mod.folder.model.block");
+        localizablePathMap.put(Paths.get("resources/textures"), "mod.folder.texture");
+        localizablePathMap.put(Paths.get("resources/textures/armor"), "mod.folder.texture.armor");
+        localizablePathMap.put(Paths.get("resources/textures/armors"), "mod.folder.texture.armor");
+        localizablePathMap.put(Paths.get("resources/textures/block"), "mod.folder.texture.block");
+        localizablePathMap.put(Paths.get("resources/textures/blocks"), "mod.folder.texture.block");
+        localizablePathMap.put(Paths.get("resources/textures/item"), "mod.folder.texture.item");
+        localizablePathMap.put(Paths.get("resources/textures/items"), "mod.folder.texture.item");
+        localizablePathMap.put(Paths.get("resources/textures/gui"), "mod.folder.texture.gui");
     }
 
     @Override
     public boolean apply(Path file, FileCell cell) {
+        final Project project = ProjectManager.getInstance().getProject(file);
+        if (project == null) return false;
+
+        final String fileName = FileUtils.getFileName(file);
         if (Files.isDirectory(file)) {
-            for (Path path : translationKeyMap.keySet()) {
-                if (file.endsWith(path)) {
-                    cell.setText(I18n.translate(translationKeyMap.get(path)));
+            final Path relativePath = project.getPath().relativize(file);
+            for (Path path : localizablePathMap.keySet()) {
+                if (relativePath.equals(path)) {
+                    cell.setText(I18n.translate(localizablePathMap.get(path), fileName));
                     cell.setIcon(Icons.File.Folder);
                     return true;
                 }
             }
         } else {
-            String fileName = FileUtils.getFileName(file);
             if (McModMetadata.FILE_NAME.equals(fileName)) {
                 cell.setText(I18n.translate("mod.file.metadata"));
                 cell.setIcon(Icons.File.Forge);
