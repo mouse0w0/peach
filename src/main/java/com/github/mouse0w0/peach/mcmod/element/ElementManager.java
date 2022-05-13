e976800e916f67d0a0e362f53a8c91e73581e1aa
Mouse <mouse0w0@qq.com>
2022-05-13 20:08:56 +0800

    fix: no clear element index content when delete element

--- src/main/java/com/github/mouse0w0/peach/mcmod/element/ElementManager.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/element/ElementManager.java
@@ -3,6 +3,9 @@ package com.github.mouse0w0.peach.mcmod.element;
 import com.github.mouse0w0.i18n.I18n;
 import com.github.mouse0w0.peach.dialog.Alert;
 import com.github.mouse0w0.peach.fileEditor.FileEditorManager;
+import com.github.mouse0w0.peach.fileWatch.FileChangeListener;
+import com.github.mouse0w0.peach.fileWatch.ProjectFileWatcher;
+import com.github.mouse0w0.peach.fileWatch.WeakFileChangeListener;
 import com.github.mouse0w0.peach.javafx.CachedImage;
 import com.github.mouse0w0.peach.mcmod.*;
 import com.github.mouse0w0.peach.mcmod.element.impl.MEItem;
@@ -12,6 +15,7 @@ import com.github.mouse0w0.peach.mcmod.index.IndexProvider;
 import com.github.mouse0w0.peach.mcmod.index.Indexes;
 import com.github.mouse0w0.peach.mcmod.project.McModDescriptor;
 import com.github.mouse0w0.peach.mcmod.util.ModUtils;
+import com.github.mouse0w0.peach.mcmod.util.ResourceUtils;
 import com.github.mouse0w0.peach.project.Project;
 import com.github.mouse0w0.peach.util.FileUtils;
 import com.github.mouse0w0.peach.util.JsonUtils;
@@ -35,12 +39,10 @@ public final class ElementManager extends IndexProvider {
     private final Project project;
     private final McModDescriptor descriptor;
     private final ElementRegistry elementRegistry;
-
-    private final Path root;
-
+    private final Path sourcesPath;
+    private final Path previewPath;
     private final Gson gson;
-
-    private final Path previewCache;
+    private final FileChangeListener fileChangeListener;
 
     public static ElementManager getInstance(Project project) {
         return project.getService(ElementManager.class);
@@ -51,36 +53,35 @@ public final class ElementManager extends IndexProvider {
         this.project = project;
         this.descriptor = descriptor;
         this.elementRegistry = elementRegistry;
-        this.root = project.getPath().resolve("sources");
-
-        this.previewCache = project.getPath().resolve(".peach/preview");
-        FileUtils.createDirectoriesIfNotExistsSilently(previewCache);
-
-        indexManager.registerProvider(this);
-
-        gson = createGson(indexManager);
-
-        init();
-    }
-
-    private Gson createGson(IndexManager indexManager) {
-        return new GsonBuilder()
+        this.sourcesPath = ResourceUtils.getResourcePath(project, ResourceUtils.SOURCES);
+        FileUtils.createDirectoriesIfNotExistsSilently(sourcesPath);
+        this.previewPath = project.getPath().resolve(".peach/preview");
+        FileUtils.createDirectoriesIfNotExistsSilently(previewPath);
+        this.gson = new GsonBuilder()
                 .registerTypeAdapter(ItemGroup.class, new ItemGroup.Persister(indexManager.getIndex(Indexes.ITEM_GROUPS)))
                 .registerTypeAdapter(Material.class, new Material.Persister(indexManager.getIndex(Indexes.MATERIALS)))
                 .registerTypeAdapter(SoundType.class, new SoundType.Persister(indexManager.getIndex(Indexes.SOUND_TYPES)))
                 .registerTypeAdapter(MapColor.class, new MapColor.Persister(indexManager.getIndex(Indexes.MAP_COLORS)))
                 .create();
+        indexManager.registerProvider(this);
+        this.fileChangeListener = new FileChangeListener() {
+            @Override
+            public void onFileDelete(ProjectFileWatcher watcher, Path path) {
+                clearElement(path);
+            }
+        };
+        ProjectFileWatcher.getInstance(project).addListener(new WeakFileChangeListener(fileChangeListener));
+        indexElements();
     }
 
-    private void init() {
+    private void indexElements() {
         try {
-            FileUtils.createDirectoriesIfNotExists(root);
-            Iterator<Path> iterator = Files.walk(root).iterator();
+            Iterator<Path> iterator = Files.walk(sourcesPath).iterator();
             while (iterator.hasNext()) {
                 Path file = iterator.next();
                 ElementType<?> elementType = elementRegistry.getElementType(file);
                 if (elementType != null) {
-                    onUpdatedElement(loadElement(file));
+                    indexElement(loadElement(file));
                 }
             }
         } catch (IOException e) {
@@ -116,22 +117,8 @@ public final class ElementManager extends IndexProvider {
             //TODO: show dialog
         }
 
-        onUpdatedElement(element);
-    }
-
-    public void removeElement(Path file) {
-        if (!file.startsWith(root)) {
-            LOGGER.warn("Try to remove an unmanaged element {}.", file);
-            return;
-        }
-
-        try {
-            Files.deleteIfExists(file);
-        } catch (IOException e) {
-            LOGGER.error("Failed to delete element file.", e);
-        }
-
-        onRemovedElement(file);
+        clearElement(element.getFile());
+        indexElement(element);
     }
 
     public void createElement(Path path, ElementType<?> type, String name) {
@@ -147,30 +134,29 @@ public final class ElementManager extends IndexProvider {
         FileEditorManager.getInstance(project).open(file);
     }
 
-    private final Map<Path, Object> cachedElement = new HashMap<>();
+    private final Map<Path, Object> indexedElements = new HashMap<>();
 
-    private void onRemovedElement(Path file) {
-        if (!cachedElement.containsKey(file)) return;
-        ElementType<?> type = ElementRegistry.getInstance().getElementType(file);
-        if (type == ElementTypes.ITEM) {
-            ItemRef[] items = (ItemRef[]) cachedElement.remove(file);
-            for (ItemRef item : items) {
-                getIndex(Indexes.ITEMS).remove(item);
+    private void clearElement(Path file) {
+        if (indexedElements.containsKey(file)) {
+            ElementType<?> type = ElementRegistry.getInstance().getElementType(file);
+            if (type == ElementTypes.ITEM) {
+                ItemRef[] items = (ItemRef[]) indexedElements.remove(file);
+                for (ItemRef item : items) {
+                    getIndex(Indexes.ITEMS).remove(item);
+                }
+            } else if (type == ElementTypes.ITEM_GROUP) {
+                getIndex(Indexes.ITEM_GROUPS).remove((String) indexedElements.remove(file));
             }
-        } else if (type == ElementTypes.ITEM_GROUP) {
-            getIndex(Indexes.ITEM_GROUPS).remove((String) cachedElement.remove(file));
         }
     }
 
-    private void onUpdatedElement(Element element) {
-        Path file = element.getFile();
-        onRemovedElement(file);
+    private void indexElement(Element element) {
         if (element instanceof MEItem) {
             MEItem meItem = (MEItem) element;
             Item item = new Item(meItem.getIdentifier(), 0, null, false);
             item.setLocalizedText(meItem.getDisplayName());
 
-            Path previewFile = previewCache.resolve(element.getFileName() + ".png");
+            Path previewFile = previewPath.resolve(element.getFileName() + ".png");
             ElementRegistry.getInstance().getElementType(MEItem.class).generatePreview(project, meItem, previewFile);
             CachedImage cachedImage = new CachedImage(previewFile, 64, 64);
             cachedImage.invalidate();
@@ -181,14 +167,14 @@ public final class ElementManager extends IndexProvider {
             getIndex(Indexes.ITEMS).put(item1, Collections.singletonList(item));
             ItemRef item2 = ItemRef.createIgnoreMetadata(namespace + ":" + item.getId());
             getIndex(Indexes.ITEMS).put(item2, Collections.singletonList(item));
-            cachedElement.put(file, new ItemRef[]{item1, item2});
+            indexedElements.put(element.getFile(), new ItemRef[]{item1, item2});
         } else if (element instanceof MEItemGroup) {
             MEItemGroup meItemGroup = (MEItemGroup) element;
             ItemGroup itemGroup = new ItemGroup(meItemGroup.getIdentifier(), null, meItemGroup.getIcon());
             itemGroup.setLocalizedText(meItemGroup.getDisplayName());
 
             getIndex(Indexes.ITEM_GROUPS).put(meItemGroup.getIdentifier(), itemGroup);
-            cachedElement.put(file, meItemGroup.getIdentifier());
+            indexedElements.put(element.getFile(), meItemGroup.getIdentifier());
         }
     }
 }
