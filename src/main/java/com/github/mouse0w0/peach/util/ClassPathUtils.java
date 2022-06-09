06744283d19d88d05af6c9a4d919f175a6cf2128
Mouse <mouse0w0@qq.com>
2022-06-10 00:36:54 +0800

    feat: throws FileNotFoundException in ClassPathUtils#getPath

--- src/main/java/com/github/mouse0w0/peach/util/ClassPathUtils.java
+++ src/main/java/com/github/mouse0w0/peach/util/ClassPathUtils.java
@@ -1,18 +1,19 @@
 package com.github.mouse0w0.peach.util;
 
+import java.io.FileNotFoundException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ClassPathUtils {
-    public static Path getPath(String name) {
+    public static Path getPath(String name) throws FileNotFoundException {
         return getPath(name, Thread.currentThread().getContextClassLoader());
     }
 
-    public static Path getPath(String name, ClassLoader classLoader) {
+    public static Path getPath(String name, ClassLoader classLoader) throws FileNotFoundException {
         URL resource = classLoader.getResource(name);
-        if (resource == null) return null;
+        if (resource == null) throw new FileNotFoundException(name);
 
         try {
             return Paths.get(resource.toURI());
