bc5c970f5de06e2681858620e48bf15dd40fe011
Mouse <mouse0w0@qq.com>
2023-01-20 16:43:29 +0800

    style: remove final modifier at parameters of StringUtils

--- src/main/java/com/github/mouse0w0/peach/util/StringUtils.java
+++ src/main/java/com/github/mouse0w0/peach/util/StringUtils.java
@@ -4,18 +4,18 @@ import java.util.stream.Stream;
 
 public class StringUtils extends org.apache.commons.lang3.StringUtils {
 
-    public static String substringBeforeLast(final String str, final int separator) {
+    public static String substringBeforeLast(String str, int separator) {
         if (isEmpty(str)) {
             return str;
         }
-        final int pos = str.lastIndexOf(separator);
+        int pos = str.lastIndexOf(separator);
         if (pos == INDEX_NOT_FOUND) {
             return str;
         }
         return str.substring(0, pos);
     }
 
-    public static boolean hasUpperCase(final String str) {
+    public static boolean hasUpperCase(String str) {
         for (int i = 0, length = str.length(); i < length; i++) {
             if (Character.isUpperCase(str.indexOf(i))) {
                 return true;
@@ -24,7 +24,7 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
         return false;
     }
 
-    public static boolean hasLowerCase(final String str) {
+    public static boolean hasLowerCase(String str) {
         for (int i = 0, length = str.length(); i < length; i++) {
             if (Character.isLowerCase(str.indexOf(i))) {
                 return true;
@@ -33,11 +33,11 @@ public class StringUtils extends org.apache.commons.lang3.StringUtils {
         return false;
     }
 
-    public static String join(final Stream<?> stream, final char separator) {
+    public static String join(Stream<?> stream, char separator) {
         return join(stream.iterator(), separator);
     }
 
-    public static String join(final Stream<?> stream, final String separator) {
+    public static String join(Stream<?> stream, String separator) {
         return join(stream.iterator(), separator);
     }
 }
