2c017d89f9c19f557da42bed043e1d8b8d9770e8
Mouse <mouse0w0@qq.com>
2023-03-18 00:49:16 +0800

    style: update WindowStateManager

--- src/main/java/com/github/mouse0w0/peach/project/service/WindowStateManager.java
+++ src/main/java/com/github/mouse0w0/peach/project/service/WindowStateManager.java
@@ -5,6 +5,7 @@ import com.github.mouse0w0.peach.service.PersistentService;
 import com.github.mouse0w0.peach.util.JsonUtils;
 import com.github.mouse0w0.peach.util.Validate;
 import com.google.gson.JsonElement;
+import javafx.beans.InvalidationListener;
 import javafx.beans.property.ReadOnlyBooleanProperty;
 import javafx.beans.property.ReadOnlyDoubleProperty;
 import javafx.beans.value.ChangeListener;
@@ -20,47 +21,40 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class WindowStateManager implements PersistentService {
-
     private static final String STATE_ID = "StateId";
     private static final String LAST_X = "LastX";
     private static final String LAST_Y = "LastY";
 
-    private final Map<String, WindowStateBean> beanMap = new HashMap<>();
+    private final Map<String, WindowState> stateMap = new HashMap<>();
 
-    private final EventHandler<WindowEvent> showing = event -> {
-        Window window = (Window) event.getSource();
-        String stateId = (String) window.getProperties().get(STATE_ID);
-        WindowStateBean bean = beanMap.get(stateId);
-        if (bean != null) bean.apply(window);
-    };
     private final EventHandler<WindowEvent> hidden = event -> {
         Window window = (Window) event.getSource();
         String stateId = (String) window.getProperties().get(STATE_ID);
-        beanMap.computeIfAbsent(stateId, k -> new WindowStateBean()).store(window);
+        stateMap.computeIfAbsent(stateId, k -> new WindowState()).store(window);
     };
-
     private final ChangeListener<Number> xListener = (observable, oldValue, newValue) -> {
-        ReadOnlyDoubleProperty property = (ReadOnlyDoubleProperty) observable;
-        Stage stage = (Stage) property.getBean();
+        ReadOnlyDoubleProperty xProperty = (ReadOnlyDoubleProperty) observable;
+        Stage stage = (Stage) xProperty.getBean();
         if (!stage.isMaximized()) {
-            stage.getProperties().put(LAST_X, oldValue.doubleValue());
+            stage.getProperties().put(LAST_X, oldValue);
         }
     };
     private final ChangeListener<Number> yListener = (observable, oldValue, newValue) -> {
-        ReadOnlyDoubleProperty property = (ReadOnlyDoubleProperty) observable;
-        Stage stage = (Stage) property.getBean();
+        ReadOnlyDoubleProperty yProperty = (ReadOnlyDoubleProperty) observable;
+        Stage stage = (Stage) yProperty.getBean();
         if (!stage.isMaximized()) {
-            stage.getProperties().put(LAST_Y, oldValue.doubleValue());
+            stage.getProperties().put(LAST_Y, oldValue);
         }
     };
-    private final ChangeListener<Boolean> maximizedListener = (observable, oldValue, newValue) -> {
-        ReadOnlyBooleanProperty property = (ReadOnlyBooleanProperty) observable;
-        Stage stage = (Stage) property.getBean();
-        String stateId = (String) stage.getProperties().get(STATE_ID);
-        if (newValue) {
-            beanMap.computeIfAbsent(stateId, k -> new WindowStateBean()).storeWithoutMaximize(stage);
+    private final InvalidationListener maximizedListener = (observable) -> {
+        ReadOnlyBooleanProperty maximizedProperty = (ReadOnlyBooleanProperty) observable;
+        Window window = (Window) maximizedProperty.getBean();
+        String stateId = (String) window.getProperties().get(STATE_ID);
+        if (maximizedProperty.get()) {
+            stateMap.computeIfAbsent(stateId, k -> new WindowState()).storeWhenMaximized(window);
         } else {
-            beanMap.computeIfAbsent(stateId, k -> new WindowStateBean()).applyWithoutMaximized(stage);
+            WindowState state = stateMap.get(stateId);
+            if (state != null) state.applyWhenNotMaximized(window);
         }
     };
 
@@ -68,26 +62,19 @@ public class WindowStateManager implements PersistentService {
         return project.getService(WindowStateManager.class);
     }
 
-    public void register(Window window, String stateId) {
+    public void register(@Nonnull Window window, @Nonnull String stateId) {
+        Validate.notNull(window);
         Validate.notEmpty(stateId);
+
+        WindowState state = stateMap.get(stateId);
+        if (state != null) state.apply(window);
+
         window.getProperties().put(STATE_ID, stateId);
-        window.addEventFilter(WindowEvent.WINDOW_SHOWING, showing);
         window.addEventFilter(WindowEvent.WINDOW_HIDDEN, hidden);
         if (window instanceof Stage) {
             window.xProperty().addListener(xListener);
             window.yProperty().addListener(yListener);
             ((Stage) window).maximizedProperty().addListener(maximizedListener);
-        }
-    }
-
-    public void unregister(Window window) {
-        window.getProperties().remove(STATE_ID);
-        window.removeEventFilter(WindowEvent.WINDOW_SHOWING, showing);
-        window.removeEventFilter(WindowEvent.WINDOW_HIDDEN, hidden);
-        if (window instanceof Stage) {
-            window.xProperty().removeListener(xListener);
-            window.yProperty().removeListener(yListener);
-            ((Stage) window).maximizedProperty().removeListener(maximizedListener);
         }
     }
 
@@ -99,15 +86,15 @@ public class WindowStateManager implements PersistentService {
 
     @Override
     public JsonElement saveState() {
-        return JsonUtils.toJson(beanMap);
+        return JsonUtils.toJson(stateMap);
     }
 
     @Override
     public void loadState(JsonElement jsonElement) {
-        beanMap.putAll(JsonUtils.fromJson(jsonElement, TypeUtils.parameterize(Map.class, String.class, WindowStateBean.class)));
+        stateMap.putAll(JsonUtils.fromJson(jsonElement, TypeUtils.parameterize(Map.class, String.class, WindowState.class)));
     }
 
-    public static class WindowStateBean {
+    private static final class WindowState {
         private double x;
         private double y;
         private double width;
@@ -126,24 +113,27 @@ public class WindowStateManager implements PersistentService {
             }
         }
 
-        public void storeWithoutMaximize(Window window) {
+        public void storeWhenMaximized(Window window) {
             ObservableMap<Object, Object> properties = window.getProperties();
-            x = properties.containsKey(LAST_X) ? (double) properties.get(LAST_X) : window.getX();
-            y = properties.containsKey(LAST_Y) ? (double) properties.get(LAST_Y) : window.getY();
+            Double lastX = (Double) properties.get(LAST_X);
+            Double lastY = (Double) properties.get(LAST_Y);
+            x = lastX != null ? lastX : window.getX();
+            y = lastY != null ? lastY : window.getY();
             width = window.getWidth();
             height = window.getHeight();
         }
 
         public void apply(Window window) {
-            if (window instanceof Stage) {
-                ((Stage) window).setMaximized(maximized);
-            }
-            if (!maximized) {
-                applyWithoutMaximized(window);
+            if (maximized) {
+                if (window instanceof Stage) {
+                    ((Stage) window).setMaximized(true);
+                }
+            } else {
+                applyWhenNotMaximized(window);
             }
         }
 
-        public void applyWithoutMaximized(Window window) {
+        public void applyWhenNotMaximized(Window window) {
             window.setX(x);
             window.setY(y);
             window.setWidth(width);
