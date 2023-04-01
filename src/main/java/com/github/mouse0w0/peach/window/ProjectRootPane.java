e5b9fca0eb28302d0e4badda292ab8ad50102e79
Mouse <mouse0w0@qq.com>
2023-04-01 15:29:04 +0800

    fix: fix menu item in submenu cannot display description in status bar

--- src/main/java/com/github/mouse0w0/peach/window/ProjectRootPane.java
+++ src/main/java/com/github/mouse0w0/peach/window/ProjectRootPane.java
@@ -4,7 +4,9 @@ import com.github.mouse0w0.peach.action.*;
 import com.github.mouse0w0.peach.javafx.FXUtils;
 import com.github.mouse0w0.peach.javafx.control.ViewPane;
 import com.github.mouse0w0.peach.project.Project;
-import javafx.event.Event;
+import com.sun.javafx.scene.control.ContextMenuContent;
+import com.sun.javafx.scene.control.MenuBarButton;
+import javafx.collections.ListChangeListener;
 import javafx.event.EventHandler;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -44,6 +46,27 @@ class ProjectRootPane extends BorderPane {
         setBottom(statusBar.getContent());
 
         tabPane.setTabClosingPolicy(TabPane.TabClosingPolicy.ALL_TABS);
+
+        Window.getWindows().addListener((ListChangeListener<Window>) c -> {
+            while (c.next()) {
+                if (c.wasAdded()) {
+                    var windows = c.getList();
+                    var from = c.getFrom();
+                    var to = c.getTo();
+                    for (int i = from; i < to; i++) {
+                        Window window = windows.get(i);
+                        if (window instanceof ContextMenu contextMenu) {
+                            Node ownerNode = contextMenu.getOwnerNode();
+                            if (ownerNode instanceof MenuBarButton button && button.menu instanceof ActionControl) {
+                                applyStatusBarInfo(contextMenu);
+                            } else if (ownerNode instanceof ContextMenuContent.MenuItemContainer container && container.getItem() instanceof ActionControl) {
+                                applyStatusBarInfo(contextMenu);
+                            }
+                        }
+                    }
+                }
+            }
+        });
     }
 
     public Project getProject() {
@@ -63,63 +86,39 @@ class ProjectRootPane extends BorderPane {
     }
 
     private MenuBar createMenuBar() {
-        MenuBar menuBar = new MenuBar();
         ActionManager actionManager = ActionManager.getInstance();
-        ActionGroup group = (ActionGroup) actionManager.getAction(ActionGroups.MAIN_MENU);
-
-        for (Action child : group.getChildren()) {
-            if (!(child instanceof ActionGroup)) continue;
-
-            Menu menu = actionManager.createMenu((ActionGroup) child);
-            setOnShownEventHandler(menu);
-            menuBar.getMenus().add(menu);
+        ActionGroup mainMenuGroup = actionManager.getActionGroup(ActionGroups.MAIN_MENU);
+        MenuBar menuBar = new MenuBar();
+        for (Action child : mainMenuGroup.getChildren()) {
+            if (child instanceof ActionGroup group) {
+                Menu menu = actionManager.createMenu(group);
+                menuBar.getMenus().add(menu);
+            }
         }
         return menuBar;
     }
 
-    private static final EventHandler<Event> ON_MENU_SHOWN = new EventHandler<>() {
-        @Override
-        public void handle(Event event) {
-            for (Window window : Window.getWindows()) {
-                if (window instanceof ContextMenu contextMenu) {
-                    Parent root = contextMenu.getScene().getRoot();
-                    for (Node node : root.lookupAll(".menu-item")) {
-                        node.setOnMouseEntered(ON_MOUSE_ENTERED);
-                        node.setOnMouseExited(ON_MOUSE_EXITED);
-                    }
-                    return;
-                }
+    private static void applyStatusBarInfo(ContextMenu contextMenu) {
+        Parent root = contextMenu.getScene().getRoot();
+        for (Node node : root.lookupAll(".menu-item")) {
+            MenuItem menuItem = FXUtils.getProperty(node, MenuItem.class);
+            if (menuItem instanceof ActionControl actionControl && actionControl.getAction().getDescription() != null) {
+                node.setOnMouseEntered(ON_MOUSE_ENTERED);
+                node.setOnMouseExited(ON_MOUSE_EXITED);
             }
         }
-    };
+    }
 
-    private static final EventHandler<? super MouseEvent> ON_MOUSE_ENTERED = new EventHandler<>() {
-        @Override
-        public void handle(MouseEvent event) {
-            Node node = (Node) event.getTarget();
-            MenuItem menuItem = (MenuItem) node.getProperties().get(MenuItem.class);
-            if (menuItem instanceof ActionControl) {
-                Action action = ((ActionControl) menuItem).getAction();
-                StatusBarInfo.getInstance(WindowManager.getInstance().getFocusedProject()).setText(action.getDescription());
-            } else {
-                StatusBarInfo.getInstance(WindowManager.getInstance().getFocusedProject()).setText(null);
-            }
-        }
-    };
-
-    private static final EventHandler<? super MouseEvent> ON_MOUSE_EXITED = new EventHandler<>() {
-        @Override
-        public void handle(MouseEvent event) {
+    private static final EventHandler<? super MouseEvent> ON_MOUSE_ENTERED = (EventHandler<MouseEvent>) event -> {
+        Node node = (Node) event.getTarget();
+        MenuItem menuItem = (MenuItem) node.getProperties().get(MenuItem.class);
+        if (menuItem instanceof ActionControl actionControl) {
+            Action action = actionControl.getAction();
+            StatusBarInfo.getInstance(WindowManager.getInstance().getFocusedProject()).setText(action.getDescription());
+        } else {
             StatusBarInfo.getInstance(WindowManager.getInstance().getFocusedProject()).setText(null);
         }
     };
 
-    private static void setOnShownEventHandler(Menu menu) {
-        menu.setOnShown(ON_MENU_SHOWN);
-        for (MenuItem item : menu.getItems()) {
-            if (item instanceof Menu) {
-                setOnShownEventHandler((Menu) item);
-            }
-        }
-    }
+    private static final EventHandler<? super MouseEvent> ON_MOUSE_EXITED = (EventHandler<MouseEvent>) event -> StatusBarInfo.getInstance(WindowManager.getInstance().getFocusedProject()).setText(null);
 }
