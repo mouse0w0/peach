fe46c9013ea35cc300a508b166842a9d10cd1f2c
Mouse <mouse0w0@qq.com>
2023-04-19 21:17:06 +0800

    style: add ExtensionPointName.toString

--- src/main/java/com/github/mouse0w0/peach/extension/ExtensionPointName.java
+++ src/main/java/com/github/mouse0w0/peach/extension/ExtensionPointName.java
@@ -27,4 +27,9 @@ public final class ExtensionPointName<T> {
     public List<T> getExtensions() {
         return getPoint().getExtensions();
     }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
