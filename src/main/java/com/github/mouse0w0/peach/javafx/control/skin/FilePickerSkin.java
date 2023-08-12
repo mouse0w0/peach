92f6089c78fceb8284d7cbe93c759200c149d2c5
Mouse <mouse0w0@qq.com>
2023-08-12 22:43:59 +0800

    Update FilePickerSkin and FilePicker.css

--- src/main/java/com/github/mouse0w0/peach/javafx/control/skin/FilePickerSkin.java
+++ src/main/java/com/github/mouse0w0/peach/javafx/control/skin/FilePickerSkin.java
@@ -8,14 +8,15 @@ import javafx.scene.AccessibleRole;
 import javafx.scene.Cursor;
 import javafx.scene.control.SkinBase;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 
 public class FilePickerSkin extends SkinBase<FilePicker> {
     private static final PseudoClass FOCUSED = PseudoClass.getPseudoClass("focused");
 
     private final TextField editor;
-    private final StackPane clearBtn;
-    private final StackPane openChooserBtn;
+    private final StackPane clearButton;
+    private final StackPane openButton;
 
     public FilePickerSkin(FilePicker filePicker) {
         super(filePicker);
@@ -29,47 +30,47 @@ public class FilePickerSkin extends SkinBase<FilePicker> {
         editor.focusedProperty().addListener(observable ->
                 pseudoClassStateChanged(FOCUSED, editor.isFocused()));
 
-        StackPane clearIcon = new StackPane();
-        clearIcon.getStyleClass().setAll("clear-icon");
+        Region clear = new Region();
+        clear.getStyleClass().add("clear");
 
-        clearBtn = new StackPane(clearIcon);
-        clearBtn.getStyleClass().setAll("clear-button");
-        clearBtn.setAccessibleRole(AccessibleRole.BUTTON);
-        clearBtn.setCursor(Cursor.HAND);
-        clearBtn.visibleProperty().bind(filePicker.valueProperty().isNotEmpty());
-        clearBtn.setOnMousePressed(event -> {
+        clearButton = new StackPane(clear);
+        clearButton.getStyleClass().add("clear-button");
+        clearButton.setAccessibleRole(AccessibleRole.BUTTON);
+        clearButton.setCursor(Cursor.HAND);
+        clearButton.visibleProperty().bind(filePicker.valueProperty().isNotEmpty());
+        clearButton.setOnMousePressed(event -> {
             event.consume();
             getSkinnable().setValue("");
         });
 
-        StackPane openChooserIcon = new StackPane();
-        openChooserIcon.getStyleClass().setAll("open-icon");
+        Region open = new Region();
+        open.getStyleClass().add("open");
 
-        openChooserBtn = new StackPane(openChooserIcon);
-        openChooserBtn.getStyleClass().setAll("open-button");
-        openChooserBtn.setAccessibleRole(AccessibleRole.BUTTON);
-        openChooserBtn.setCursor(Cursor.HAND);
-        openChooserBtn.setOnMousePressed(event -> {
+        openButton = new StackPane(open);
+        openButton.getStyleClass().add("open-button");
+        openButton.setAccessibleRole(AccessibleRole.BUTTON);
+        openButton.setCursor(Cursor.HAND);
+        openButton.setOnMousePressed(event -> {
             event.consume();
             getSkinnable().showDialog();
         });
 
-        getChildren().addAll(editor, clearBtn, openChooserBtn);
+        getChildren().addAll(editor, clearButton, openButton);
     }
 
     @Override
     protected double computePrefWidth(double height, double topInset, double rightInset, double bottomInset, double leftInset) {
         final double editorWidth = snapSizeX(editor.prefWidth(-1));
-        final double clearBtnWidth = snapSizeX(clearBtn.prefWidth(-1));
-        final double openChooserBtnWidth = snapSizeX(openChooserBtn.prefWidth(-1));
+        final double clearBtnWidth = snapSizeX(clearButton.prefWidth(-1));
+        final double openChooserBtnWidth = snapSizeX(openButton.prefWidth(-1));
         return leftInset + editorWidth + clearBtnWidth + openChooserBtnWidth + rightInset;
     }
 
     @Override
     protected double computePrefHeight(double width, double topInset, double rightInset, double bottomInset, double leftInset) {
         final double editorHeight = snapSizeY(editor.prefHeight(-1));
-        final double clearBtnHeight = snapSizeY(clearBtn.prefHeight(-1));
-        final double openChooserBtnHeight = snapSizeY(openChooserBtn.prefHeight(-1));
+        final double clearBtnHeight = snapSizeY(clearButton.prefHeight(-1));
+        final double openChooserBtnHeight = snapSizeY(openButton.prefHeight(-1));
         return topInset + Math.max(Math.max(editorHeight, clearBtnHeight), openChooserBtnHeight) + bottomInset;
     }
 
@@ -80,18 +81,18 @@ public class FilePickerSkin extends SkinBase<FilePicker> {
 
     @Override
     protected double computeMaxHeight(double width, double topInset, double rightInset, double bottomInset, double leftInset) {
-        return getSkinnable().prefHeight(width);
+        return computePrefHeight(width, topInset, rightInset, bottomInset, leftInset);
     }
 
     @Override
     protected void layoutChildren(double contentX, double contentY, double contentWidth, double contentHeight) {
-        final double openChooserBtnWidth = snapSizeX(openChooserBtn.prefWidth(-1));
-        final double clearBtnWidth = snapSizeX(clearBtn.prefWidth(-1));
+        final double openChooserBtnWidth = snapSizeX(openButton.prefWidth(-1));
+        final double clearBtnWidth = snapSizeX(clearButton.prefWidth(-1));
         final double editorWidth = contentWidth - clearBtnWidth - openChooserBtnWidth;
         layoutInArea(editor, contentX, contentY, editorWidth, contentHeight, 0, HPos.LEFT, VPos.CENTER);
-        layoutInArea(clearBtn, contentX + editorWidth, contentY,
+        layoutInArea(clearButton, contentX + editorWidth, contentY,
                 clearBtnWidth, contentHeight, 0, HPos.CENTER, VPos.CENTER);
-        layoutInArea(openChooserBtn, contentX + editorWidth + clearBtnWidth, contentY,
+        layoutInArea(openButton, contentX + editorWidth + clearBtnWidth, contentY,
                 openChooserBtnWidth, contentHeight, 0, HPos.CENTER, VPos.CENTER);
     }
 }
