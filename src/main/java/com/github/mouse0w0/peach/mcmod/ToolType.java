247f75cc81e1a4c86e5dbf1ff768bca658cec0de
Mouse <mouse0w0@qq.com>
2023-10-10 22:54:49 +0800

    Update ToolType.getToolTypes

--- src/main/java/com/github/mouse0w0/peach/mcmod/ToolType.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/ToolType.java
@@ -1,12 +1,12 @@
 package com.github.mouse0w0.peach.mcmod;
 
 import com.github.mouse0w0.peach.l10n.AppL10n;
-import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.ImmutableList;
 
-import java.util.Set;
+import java.util.List;
 
 public final class ToolType {
-    private static final Set<String> TOOL_TYPES;
+    private static final List<String> TOOL_TYPES;
 
     public static final String NONE = "none";
     public static final String AXE = "axe";
@@ -15,10 +15,10 @@ public final class ToolType {
     public static final String HOE = "hoe";
 
     static {
-        TOOL_TYPES = ImmutableSet.of(AXE, PICKAXE, SHOVEL, HOE);
+        TOOL_TYPES = ImmutableList.of(AXE, PICKAXE, SHOVEL, HOE);
     }
 
-    public static Set<String> getToolTypes() {
+    public static List<String> getToolTypes() {
         return TOOL_TYPES;
     }
 
