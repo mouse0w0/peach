00fe26d4c2312da6f41987a8893c602e8d8184d0
Mouse <mouse0w0@qq.com>
2023-11-10 15:57:33 +0800

    Update Zip

--- src/main/java/com/github/mouse0w0/peach/mcmod/generator/task/Zip.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/generator/task/Zip.java
@@ -2,56 +2,61 @@ package com.github.mouse0w0.peach.mcmod.generator.task;
 
 import com.github.mouse0w0.peach.mcmod.generator.Context;
 import com.github.mouse0w0.peach.util.FileUtils;
-import com.google.common.collect.ImmutableSet;
-import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Iterator;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Stream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip implements Task {
-    private final Path output;
-    private final Set<Path> inputs;
+    private final Path outputPath;
+    private final List<Path> inputPaths = new ArrayList<>();
 
-    public Zip(Path output, Path... inputs) {
-        this.output = output;
-        this.inputs = ImmutableSet.copyOf(inputs);
+    public Zip(Path outputPath, Path... inputPaths) {
+        this.outputPath = outputPath;
+        Collections.addAll(this.inputPaths, inputPaths);
     }
 
-    public Path getOutput() {
-        return output;
+    public Zip(Path outputPath, Collection<Path> inputPaths) {
+        this.outputPath = outputPath;
+        this.inputPaths.addAll(inputPaths);
     }
 
-    public Set<Path> getInputs() {
-        return inputs;
+    public Path getOutputPath() {
+        return outputPath;
+    }
+
+    public List<Path> getInputPaths() {
+        return inputPaths;
     }
 
     @Override
     public void run(Context context) throws Exception {
-        try (ZipOutputStream output = new ZipOutputStream(FileUtils.newOutputStream(this.output))) {
-            for (Path input : inputs) {
-                Path root = Files.isRegularFile(input) ? input.getParent() : input;
-                Iterator<Path> iterator = Files.walk(input).iterator();
-                while (iterator.hasNext()) {
-                    Path file = iterator.next();
+        try (ZipOutputStream output = new ZipOutputStream(FileUtils.newOutputStream(outputPath))) {
+            for (Path input : inputPaths) {
+                if (Files.isDirectory(input)) {
+                    try (Stream<Path> walk = Files.walk(input)) {
+                        Iterator<Path> iterator = walk.iterator();
+                        while (iterator.hasNext()) {
+                            Path file = iterator.next();
 
-                    if (Files.isRegularFile(file)) {
-                        copyFileIntoZip(file, root.relativize(file).toString().replace('\\', '/'), output);
+                            if (Files.isRegularFile(file)) {
+                                putFileIntoZip(input.relativize(file).toString().replace('\\', '/'), file, output);
+                            }
+                        }
                     }
+                } else if (Files.isRegularFile(input)) {
+                    putFileIntoZip(FileUtils.getFileName(input), input, output);
                 }
             }
         }
     }
 
-    private static void copyFileIntoZip(Path file, String entryName, ZipOutputStream output) throws IOException {
-        try (InputStream input = Files.newInputStream(file)) {
-            output.putNextEntry(new ZipEntry(entryName));
-            IOUtils.copy(input, output);
-        }
+    private static void putFileIntoZip(String entryName, Path source, ZipOutputStream output) throws IOException {
+        output.putNextEntry(new ZipEntry(entryName));
+        Files.copy(source, output);
     }
 }
