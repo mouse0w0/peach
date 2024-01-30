9eac504645f025e203d8be5fd69cfb04fdda61f5
Mouse <mouse0w0@qq.com>
2024-01-30 15:13:59 +0800

    Fix cannot remove recent project item

--- src/main/java/com/github/mouse0w0/peach/recentProject/RecentProjectRemoveAction.java
+++ src/main/java/com/github/mouse0w0/peach/recentProject/RecentProjectRemoveAction.java
@@ -4,8 +4,10 @@ import com.github.mouse0w0.peach.action.Action;
 import com.github.mouse0w0.peach.action.ActionEvent;
 import com.github.mouse0w0.peach.action.ActionMenuItem;
 import com.github.mouse0w0.peach.data.DataKeys;
+import com.github.mouse0w0.peach.data.DataManagerImpl;
 import javafx.scene.Node;
 import javafx.scene.control.ListCell;
+import javafx.stage.PopupWindow;
 
 public final class RecentProjectRemoveAction extends Action {
     @Override
@@ -13,7 +15,15 @@ public final class RecentProjectRemoveAction extends Action {
         RecentProjectInfo info = (RecentProjectInfo) DataKeys.SELECTED_ITEM.get(event);
         RecentProjectManager.getInstance().removeRecentProject(info.getPath());
         ActionMenuItem source = (ActionMenuItem) event.getEvent().getSource();
-        ListCell<RecentProjectInfo> listCell = (ListCell<RecentProjectInfo>) source.getParentPopup().getProperties().get(Node.class);
+        ListCell<RecentProjectInfo> listCell = (ListCell<RecentProjectInfo>) getOwnerNode(source.getParentPopup());
         listCell.getListView().getItems().remove(info);
+    }
+
+    private static Node getOwnerNode(PopupWindow popupWindow) {
+        Node ownerNode = popupWindow.getOwnerNode();
+        if (ownerNode != null) {
+            return ownerNode;
+        }
+        return (Node) popupWindow.getProperties().get(DataManagerImpl.OWNER_NODE);
     }
 }
