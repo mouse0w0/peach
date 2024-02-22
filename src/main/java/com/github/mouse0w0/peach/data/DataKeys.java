3e85d474ce0a62edb8e16fe8da12ef7b27d043d4
Mouse <mouse0w0@qq.com>
2024-02-22 17:06:05 +0800

    Remove DataKeys.SELECTED_ITEM and DataKeys.SELECTED_ITEMS

--- src/main/java/com/github/mouse0w0/peach/data/DataKeys.java
+++ src/main/java/com/github/mouse0w0/peach/data/DataKeys.java
@@ -8,9 +8,6 @@ import java.util.List;
 public interface DataKeys {
     DataKey<Project> PROJECT = DataKey.create("Project");
 
-    DataKey<Object> SELECTED_ITEM = DataKey.create("SelectedItem");
-    DataKey<List<?>> SELECTED_ITEMS = DataKey.create("SelectedItems");
-
     DataKey<Path> PATH = DataKey.create("Path");
     DataKey<List<Path>> PATHS = DataKey.create("Paths");
 }
