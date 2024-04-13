91e85d94a174faac6ea0433182b80f85da633fd0
Mouse <mouse0w0@qq.com>
2024-04-14 00:07:51 +0800

    Rename Icon.apply to Icon.setIcon

--- src/main/java/com/github/mouse0w0/peach/icon/Icon.java
+++ src/main/java/com/github/mouse0w0/peach/icon/Icon.java
@@ -9,7 +9,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public interface Icon {
-    static void apply(ObjectProperty<Node> graphic, Icon icon) {
+    static void setIcon(ObjectProperty<Node> graphic, Icon icon) {
         if (graphic.get() instanceof ImageView imageView) {
             imageView.setImage(icon != null ? icon.getImage() : null);
         } else if (icon != null) {
