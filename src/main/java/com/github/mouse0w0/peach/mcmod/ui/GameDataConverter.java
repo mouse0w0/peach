0e3c97daf775af7a8218b43e33e0188210f4d561
Mouse <mouse0w0@qq.com>
2024-04-16 16:56:26 +0800

    Update GameDataConverter

--- src/main/java/com/github/mouse0w0/peach/mcmod/ui/GameDataConverter.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/ui/GameDataConverter.java
@@ -4,29 +4,29 @@ import com.github.mouse0w0.peach.mcmod.GameData;
 import com.github.mouse0w0.peach.mcmod.index.Index;
 import javafx.util.StringConverter;
 
-public class GameDataConverter extends StringConverter<String> {
-    private final Index<String, GameData> index;
+public class GameDataConverter<T> extends StringConverter<T> {
+    private final Index<T, GameData> index;
 
-    public static GameDataConverter create(Index<String, GameData> index) {
-        return new GameDataConverter(index);
+    public static <T> GameDataConverter<T> create(Index<T, GameData> index) {
+        return new GameDataConverter<>(index);
     }
 
-    private GameDataConverter(Index<String, GameData> index) {
+    private GameDataConverter(Index<T, GameData> index) {
         this.index = index;
     }
 
     @Override
-    public String toString(String object) {
+    public String toString(T object) {
         GameData gameData = index.get(object);
         if (gameData != null) {
             return gameData.getName();
         } else {
-            return object;
+            return object.toString();
         }
     }
 
     @Override
-    public String fromString(String string) {
-        return null;
+    public T fromString(String string) {
+        throw new UnsupportedOperationException();
     }
 }
