d6ed8d3d5a3cbce4049f61082b40bd83076f0b64
Mouse <mouse0w0@qq.com>
2025-02-26 00:39:17 +0800

    Rewrite ItemView

--- src/main/java/com/github/mouse0w0/peach/mcmod/ui/control/ItemView.java
+++ src/main/java/com/github/mouse0w0/peach/mcmod/ui/control/ItemView.java
@@ -3,23 +3,42 @@ package com.github.mouse0w0.peach.mcmod.ui.control;
 import com.github.mouse0w0.peach.mcmod.IdMetadata;
 import com.github.mouse0w0.peach.mcmod.Identifier;
 import com.github.mouse0w0.peach.mcmod.ItemData;
+import com.github.mouse0w0.peach.mcmod.index.Index;
+import com.github.mouse0w0.peach.mcmod.index.IndexKeys;
+import com.github.mouse0w0.peach.mcmod.index.IndexManager;
 import com.github.mouse0w0.peach.mcmod.project.ModProjectService;
 import com.github.mouse0w0.peach.mcmod.tooltip.ItemTooltipProvider;
-import com.github.mouse0w0.peach.mcmod.ui.control.skin.ItemViewSkin;
+import com.github.mouse0w0.peach.mcmod.util.ResourceUtils;
 import com.github.mouse0w0.peach.project.Project;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.beans.property.*;
-import javafx.scene.control.Control;
-import javafx.scene.control.Skin;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Region;
+import javafx.util.Duration;
 
 import java.util.List;
 
-public class ItemView extends Control implements ItemTooltipProvider {
+public class ItemView extends Region implements ItemTooltipProvider {
     private final Project project;
+    private final Index<IdMetadata, List<ItemData>> index;
+    private final ImageView imageView;
 
     public ItemView(Project project) {
         this.project = project;
         getStyleClass().add("item-view");
         setPickOnBounds(true);
+
+        this.index = IndexManager.getInstance(project).getIndex(IndexKeys.ITEM);
+        this.imageView = new ImageView();
+        imageView.fitWidthProperty().bind(sizeProperty());
+        imageView.fitHeightProperty().bind(sizeProperty());
+        getChildren().add(imageView);
+
+        sizeProperty().addListener(observable -> requestLayout());
+        itemProperty().addListener(observable -> updateItem());
+        playAnimationProperty().addListener(observable -> updateItem());
     }
 
     public ItemView(Project project, double size) {
@@ -82,25 +101,99 @@ public class ItemView extends Control implements ItemTooltipProvider {
         playAnimationProperty().set(playAnimation);
     }
 
-    public final void resetAnimation() {
-        Skin<?> skin = getSkin();
-        if (skin != null) {
-            ((ItemViewSkin) skin).resetAnimation();
-        }
+    @Override
+    protected double computeMinWidth(double height) {
+        return snappedLeftInset() + imageView.minWidth(-1) + snappedRightInset();
     }
 
     @Override
-    protected Skin<?> createDefaultSkin() {
-        return new ItemViewSkin(this);
+    protected double computeMinHeight(double width) {
+        return snappedTopInset() + imageView.minHeight(-1) + snappedBottomInset();
     }
 
-    private ItemViewSkin getItemViewSkin() {
-        return (ItemViewSkin) getSkin();
+    @Override
+    protected double computePrefWidth(double height) {
+        return snappedLeftInset() + imageView.prefWidth(-1) + snappedRightInset();
+    }
+
+    @Override
+    protected double computePrefHeight(double width) {
+        return snappedTopInset() + imageView.prefHeight(-1) + snappedBottomInset();
+    }
+
+    @Override
+    protected double computeMaxWidth(double height) {
+        return snappedLeftInset() + imageView.maxWidth(-1) + snappedRightInset();
+    }
+
+    @Override
+    protected double computeMaxHeight(double width) {
+        return snappedTopInset() + imageView.maxHeight(-1) + snappedBottomInset();
+    }
+
+    @Override
+    protected void layoutChildren() {
+        final double x = snappedLeftInset();
+        final double y = snappedTopInset();
+        final double w = snapSizeX(getWidth()) - x - snappedRightInset();
+        final double h = snapSizeY(getHeight()) - y - snappedBottomInset();
+        imageView.resizeRelocate(x, y, w, h);
+    }
+
+    private List<ItemData> itemDataList;
+    private int currentItemIndex;
+    private Animation animation;
+
+    private void updateItem() {
+        if (animation != null) {
+            animation.stop();
+        }
+
+        IdMetadata item = getItem();
+        if (item == null) {
+            imageView.setImage(null);
+            return;
+        }
+
+        itemDataList = index.get(item);
+
+        if (itemDataList == null || itemDataList.isEmpty()) {
+            imageView.setImage(ResourceUtils.MISSING_TEXTURE);
+            return;
+        }
+
+        imageView.setImage(itemDataList.getFirst().getTexture());
+
+        if (isPlayAnimation() && itemDataList.size() > 1) {
+            currentItemIndex = 0;
+            if (animation == null) {
+                animation = new Timeline(new KeyFrame(Duration.seconds(1), event -> {
+                    currentItemIndex++;
+                    if (currentItemIndex >= itemDataList.size()) {
+                        currentItemIndex = 0;
+                    }
+                    imageView.setImage(itemDataList.get(currentItemIndex).getTexture());
+                }));
+                animation.setCycleCount(Animation.INDEFINITE);
+            }
+            animation.play();
+        }
+    }
+
+    public void resetAnimation() {
+        if (animation == null) return;
+        if (animation.getStatus() != Animation.Status.RUNNING) return;
+
+        currentItemIndex = 0;
+        imageView.setImage(itemDataList.getFirst().getTexture());
+        animation.jumpTo(Duration.ZERO);
     }
 
     @Override
     public void addToTooltip(List<String> tooltips) {
         IdMetadata item = getItem();
+        if (item == null) return;
+
         Identifier id = item.getIdentifier();
 
         StringBuilder sb = new StringBuilder();
@@ -120,7 +213,6 @@ public class ItemView extends Control implements ItemTooltipProvider {
         tooltips.add(sb.toString());
         tooltips.add("--------------------");
 
-        List<ItemData> itemDataList = getItemViewSkin().getIndex().get(item);
         if (itemDataList != null) {
             for (ItemData itemData : itemDataList) {
                 tooltips.add(itemData.getName());
