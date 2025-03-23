eb93f42316dd18e95074355e020ca65b4696ff86
Mouse <mouse0w0@qq.com>
2025-03-23 15:55:23 +0800

    Add delegate methods for valueFactory in IntegerSpinner and DoubleSpinner

--- src/main/java/com/github/mouse0w0/peach/ui/control/DoubleSpinner.java
+++ src/main/java/com/github/mouse0w0/peach/ui/control/DoubleSpinner.java
@@ -1,9 +1,12 @@
 package com.github.mouse0w0.peach.ui.control;
 
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.util.StringConverter;
 
 public class DoubleSpinner extends Spinner<Double> {
     private final SpinnerValueFactory.DoubleSpinnerValueFactory valueFactory;
@@ -37,6 +40,66 @@ public class DoubleSpinner extends Spinner<Double> {
 
     public final void setValue(Double value) {
         valueFactory.setValue(value);
+    }
+
+    public final DoubleProperty minProperty() {
+        return valueFactory.minProperty();
+    }
+
+    public final double getMin() {
+        return valueFactory.getMin();
+    }
+
+    public final void setMin(double value) {
+        valueFactory.setMin(value);
+    }
+
+    public final DoubleProperty maxProperty() {
+        return valueFactory.maxProperty();
+    }
+
+    public final double getMax() {
+        return valueFactory.getMax();
+    }
+
+    public final void setMax(double value) {
+        valueFactory.setMax(value);
+    }
+
+    public final DoubleProperty amountToStepByProperty() {
+        return valueFactory.amountToStepByProperty();
+    }
+
+    public final double getAmountToStepBy() {
+        return valueFactory.getAmountToStepBy();
+    }
+
+    public final void setAmountToStepBy(double value) {
+        valueFactory.setAmountToStepBy(value);
+    }
+
+    public final ObjectProperty<StringConverter<Double>> converterProperty() {
+        return valueFactory.converterProperty();
+    }
+
+    public final StringConverter<Double> getConverter() {
+        return valueFactory.getConverter();
+    }
+
+    public final void setConverter(StringConverter<Double> newValue) {
+        valueFactory.setConverter(newValue);
+    }
+
+    public final BooleanProperty wrapAroundProperty() {
+        return valueFactory.wrapAroundProperty();
+    }
+
+    public final boolean isWrapAround() {
+        return valueFactory.isWrapAround();
+    }
+
+    public final void setWrapAround(boolean value) {
+        valueFactory.setWrapAround(value);
     }
 
     private final DoubleProperty defaultValue = new SimpleDoubleProperty(this, "defaultValue");
--- src/main/java/com/github/mouse0w0/peach/ui/control/IntegerSpinner.java
+++ src/main/java/com/github/mouse0w0/peach/ui/control/IntegerSpinner.java
@@ -1,9 +1,12 @@
 package com.github.mouse0w0.peach.ui.control;
 
+import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.SpinnerValueFactory;
+import javafx.util.StringConverter;
 
 public class IntegerSpinner extends Spinner<Integer> {
     private final SpinnerValueFactory.IntegerSpinnerValueFactory valueFactory;
@@ -37,6 +40,66 @@ public class IntegerSpinner extends Spinner<Integer> {
 
     public final void setValue(Integer value) {
         valueFactory.setValue(value);
+    }
+
+    public final IntegerProperty minProperty() {
+        return valueFactory.minProperty();
+    }
+
+    public final int getMin() {
+        return valueFactory.getMin();
+    }
+
+    public final void setMin(int value) {
+        valueFactory.setMin(value);
+    }
+
+    public final IntegerProperty maxProperty() {
+        return valueFactory.maxProperty();
+    }
+
+    public final int getMax() {
+        return valueFactory.getMax();
+    }
+
+    public final void setMax(int value) {
+        valueFactory.setMax(value);
+    }
+
+    public final IntegerProperty amountToStepByProperty() {
+        return valueFactory.amountToStepByProperty();
+    }
+
+    public final int getAmountToStepBy() {
+        return valueFactory.getAmountToStepBy();
+    }
+
+    public final void setAmountToStepBy(int value) {
+        valueFactory.setAmountToStepBy(value);
+    }
+
+    public final ObjectProperty<StringConverter<Integer>> converterProperty() {
+        return valueFactory.converterProperty();
+    }
+
+    public final StringConverter<Integer> getConverter() {
+        return valueFactory.getConverter();
+    }
+
+    public final void setConverter(StringConverter<Integer> newValue) {
+        valueFactory.setConverter(newValue);
+    }
+
+    public final BooleanProperty wrapAroundProperty() {
+        return valueFactory.wrapAroundProperty();
+    }
+
+    public final boolean isWrapAround() {
+        return valueFactory.isWrapAround();
+    }
+
+    public final void setWrapAround(boolean value) {
+        valueFactory.setWrapAround(value);
     }
 
     private final IntegerProperty defaultValue = new SimpleIntegerProperty(this, "defaultValue");
