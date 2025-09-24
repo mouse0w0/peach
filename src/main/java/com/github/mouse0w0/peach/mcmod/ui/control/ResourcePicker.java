1c82242124119b1277f3b195e30f152a47007c2c
Mouse <mouse0w0@qq.com>
2025-09-24 20:05:01 +0800

    Add ResourcePicker

---
+++ src/main/java/com/github/mouse0w0/peach/mcmod/ui/control/ResourcePicker.java
@@ -0,0 +1,40 @@
+package com.github.mouse0w0.peach.mcmod.ui.control;
+
+import com.github.mouse0w0.peach.mcmod.util.ResourceStore;
+import com.github.mouse0w0.peach.ui.control.FilePicker;
+import javafx.util.StringConverter;
+
+import java.io.File;
+import java.util.Objects;
+
+public class ResourcePicker extends FilePicker {
+    private final ResourceStore resourceStore;
+
+    public ResourcePicker(ResourceStore resourceStore) {
+        this.resourceStore = resourceStore;
+        valueProperty().addListener(observable -> {
+            File oldFile = getFile();
+            if (oldFile == null || !oldFile.isFile()) return;
+            File newFile = resourceStore.store(oldFile);
+            if (!Objects.equals(oldFile, newFile)) {
+                setFile(newFile);
+            }
+        });
+        setConverter(new StringConverter<>() {
+
+            @Override
+            public String toString(File object) {
+                return resourceStore.relativize(object);
+            }
+
+            @Override
+            public File fromString(String string) {
+                return resourceStore.resolveToFile(string);
+            }
+        });
+    }
+
+    public ResourceStore getResourceStore() {
+        return resourceStore;
+    }
+}
